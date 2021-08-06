ea6bb2ac22668381da62155672287c68a5cfed7a
Eunice Atieno <eunniceatieno@gmail.com>
2021-08-06 13:03:12 +0300

    get object

--- study_log_app/views.py
+++ study_log_app/views.py
@@ -3,6 +3,7 @@ from .models import Topic,Entry
 from .forms import TopicForm,EntryForm
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 
@@ -18,7 +19,8 @@ def topics(request):
 
 @login_required
 def topic(request,topic_id):
-    topic=Topic.objects.get(id=topic_id)
+    # topic=Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     if topic.owner != request.user:
         raise Http404
     
